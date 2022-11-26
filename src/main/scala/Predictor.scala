75b9afaf82ff53f6a12f268f75c6b4e7f7b01338
María José Torres Nieves <majito.200130@gmail.com>
2022-11-26 08:27:11 -0500

    Codigo funcionando

--- src/main/scala/Predictor.scala
+++ src/main/scala/Predictor.scala
@@ -1,4 +1,6 @@
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.{OneHotEncoder, StandardScaler, StringIndexer, VectorAssembler}
+import org.apache.spark.ml.regression.LinearRegressionModel
 import org.apache.spark.sql.{Dataset, Encoders}
 
 object Predictor extends SparkSessionWrapper {
@@ -43,14 +45,13 @@ object Predictor extends SparkSessionWrapper {
     val dataFrame2 = vgsalesDF.drop("Rank", "Name", "Year")
     val dataFrame3 = vgsalesDF.drop("NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales")
 
-    val labels = dataFrame3.select("Global_Sales")
     val columns = Array("PlatformIndex", "GenreIndex", "PublisherIndex", "NA_Sales", "EU_Sales")
     val assembler = new VectorAssembler()
       .setInputCols(columns)
       .setOutputCol("features")
 
     val output = assembler.transform(dataFrame2)
-    output.show()
+    //output.show()
 
     val Array(training, test) = output.randomSplit(Array(0.7, 0.3), 18)
     val scaler = new StandardScaler()
@@ -60,22 +61,28 @@ object Predictor extends SparkSessionWrapper {
       .setWithMean(false)
     val scalerModel = scaler.fit(training)
     val scaledData = scalerModel.transform(training)
-    scaledData.show()
+    //scaledData.show()
 
     val scalerModelTest = scaler.fit(test)
     val scaledDataTest = scalerModelTest.transform(test)
-    scaledDataTest.show()
+    //scaledDataTest.show()
 
     val linearRegression = new LinearRegression()
 
+    linearRegression.setLabelCol("Global_Sales")
     val linearRegressionModel = linearRegression.fit(scaledData)
     val linearRegressionPredictions = linearRegressionModel.transform(scaledDataTest)
 
     linearRegressionPredictions.show()
+    linearRegressionModel.summary.residuals.show()
 
-
-
-
+    val RMSE = linearRegressionModel.summary.rootMeanSquaredError
+    println(s"Distancia media cuadratica minima: ${RMSE}")
+    val exactitud = linearRegressionModel.summary.r2
+    println(s"Exactitud:  ${exactitud*100}%")
+    val i = linearRegressionModel.intercept
+    val c = linearRegressionModel.coefficients(0)
+    print(s"Modelo matematico: Y = ${c} * X + ${i}")
 
   }
 }
